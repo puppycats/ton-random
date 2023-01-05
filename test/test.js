539572a9062f3626eb1484f8fc415885b2033ca0
Gusarich <danielsedovzzz@gmail.com>
2023-01-05 15:04:26 +0300

    it runs finally

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
-import { Cell, beginCell, WalletContractV4 } from 'ton'
+import { Cell, beginCell, WalletContractV4, toNano } from 'ton'
 import { ContractSystem, ContractExecutor, testAddress, testExternalAddress, testKey } from 'ton-emulator'
 import { compileFunc } from "@ton-community/func-js"
 import { readFileSync } from "fs"
-import { should } from 'chai'
+import { expect } from 'chai'
 
-const createMainSC = async (system) => {
+const createMainSC = async (system, treasure) => {
     const targets = {
         'stdlib.fc': 'contract/stdlib.fc',
         'opcodes.fc': 'contract/opcodes.fc',
@@ -18,11 +18,21 @@ const createMainSC = async (system) => {
 
     const code = Cell.fromBoc(Buffer.from(compilationResult.codeBoc, 'base64'))[0]
     const data = beginCell().endCell()
+    const contract = await ContractExecutor.create({ code, data, workchain: 0, balance: '0' }, system)
 
-    return ContractExecutor.create({ code, data, balance: '100000000000' }, system)
+    await treasure.send({
+        sendMode: 0,
+        to: contract.address,
+        value: toNano(10),
+        init: { code, data },
+        body: beginCell().endCell(),
+        bounce: false
+    })
+    await system.run()
+    return contract
 }
 
-const createSkipSC = async (system) => {
+const createSkipSC = async (system, treasure) => {
     const targets = {
         'stdlib.fc': 'contract/stdlib.fc',
         'opcodes.fc': 'contract/opcodes.fc',
@@ -36,8 +46,22 @@ const createSkipSC = async (system) => {
 
     const code = Cell.fromBoc(Buffer.from(compilationResult.codeBoc, 'base64'))[0]
     const data = beginCell().endCell()
+    const contract = await ContractExecutor.create({ code, data, workchain: -1, balance: '0' }, system)
 
-    return ContractExecutor.create({ code, data, workchain: -1, balance: '1000000000' }, system)
+    await treasure.send({
+        sendMode: 0,
+        to: contract.address,
+        value: toNano(10),
+        init: { code, data },
+        body: beginCell().endCell(),
+        bounce: false
+    })
+    await system.run()
+    return contract
+}
+
+const getBalance = async (system, contract) => {
+    return (await system.provider(contract).getState()).balance
 }
 
 describe('TON Random', () => {
@@ -45,12 +69,21 @@ describe('TON Random', () => {
 
     before(async () => {
         system = await ContractSystem.create()
-        treasure = await system.treasure('my-treasure')
-        skip = await createSkipSC(system)
-        main = await createMainSC(system)
+        treasure = await system.treasure('treasure')
+        skip = await createSkipSC(system, treasure)
+        main = await createMainSC(system, treasure)
     })
 
-    it('should compile', async () => {
+    it('should generate number successfully', async () => {
+        await treasure.send({
+            sendMode: 0,
+            to: skip.address,
+            value: toNano(123),
+            body: beginCell().storeUint(0x630dd32b, 32).storeUint(123, 10).endCell(),
+            bounce: true
+        })
 
+        let txs = await system.run()
+        console.log(txs)
     })
 })
